ef4145971cc9ce4f9a84d34522d515f854e08769
Aniss <aniss.sej@gmail.com>
2025-05-28 14:25:45 +0200

    init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,        // équivalent à --host 0.0.0.0
+    port: 5173,        // facultatif, port par défaut
+    strictPort: false  // si le port est occupé, cherche un port libre
+  }
+});
